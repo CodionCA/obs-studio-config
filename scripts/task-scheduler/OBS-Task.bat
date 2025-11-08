f75a5c6e9bb2414a2cc9618e84eba15693a7cbd7
CD <61989733+CodionCA@users.noreply.github.com>
2025-11-07 20:43:08 -0500

    Update paths

--- scripts/task-scheduler/OBS-Task.bat
+++ scripts/task-scheduler/OBS-Task.bat
@@ -1,7 +1,6 @@
 @echo off
 color 0B
 cls
-
 echo.
 echo   +-------------------------------------------+
 echo   ^|                                           ^|
@@ -9,13 +8,9 @@ echo   ^|        Starting OBS Studio...             ^|
 echo   ^|                                           ^|
 echo   +-------------------------------------------+
 echo.
-
 TIMEOUT 15
-
-start /d "S:\Portable\scoop\apps\obs-studio\current\bin\64bit" obs64.exe --startreplaybuffer --minimize-to-tray --disable-updater
-
+start /d "%USERPROFILE%\scoop\apps\obs-studio\current\bin\64bit" obs64.exe --startreplaybuffer --minimize-to-tray --disable-updater
 echo  OBS Studio launched successfully
 echo.
 pause >nul
-
 exit /b
